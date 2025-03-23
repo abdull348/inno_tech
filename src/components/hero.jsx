ca533341a76ae2b141ecef364719f5cd5ec6410f
A Rahman <93153465+abdull348@users.noreply.github.com>
2025-03-23 20:58:30 +0500

    react-helmet removed

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,108 +1,93 @@
-import { Helmet } from 'react-helmet-async';
 import Hero1 from '/assets/Hero1.webp';
 import Hero2 from '/assets/Hero2.webp';
 import Hero3 from '/assets/Hero3.webp';
 
 const Hero = () => {
   return (
-    <>
-      <Helmet>
-        <title>InnoTech Technologies - Innovative Lab Solutions</title>
-        <meta
-          name='description'
-          content='Welcome to InnoTech Technologies. We provide cutting-edge laboratory equipment solutions for life sciences and research'
-        />
-        <meta
-          name='keywords'
-          content='InnoTech, InnoTech Technologies, Laboratory Equipment, Science Technology, Research Solutions'
-        />
-        <link rel='canonical' href='https://innotechtechnologies.us/' />
-      </Helmet>
+    <div
+      className='container-fluid position-relative bg-light text-dark text-center p-0'
+      style={{ height: '90vh', overflow: 'hidden' }}
+    >
       <div
-        className='container-fluid position-relative bg-light text-dark text-center p-0'
-        style={{ height: '90vh', overflow: 'hidden' }}
+        id='backgroundCarousel'
+        className='carousel slide position-absolute top-0 start-0 w-100 h-80  d-none d-md-block'
+        data-bs-ride='carousel'
+        data-bs-interval='4000'
+        style={{ zIndex: 0 }}
       >
-        <div
-          id='backgroundCarousel'
-          className='carousel slide position-absolute top-0 start-0 w-100 h-80  d-none d-md-block'
-          data-bs-ride='carousel'
-          data-bs-interval='4000'
-          style={{ zIndex: 0 }}
-        >
-          <div className='carousel-inner position-relative'>
-            <div className='carousel-item' style={{ top: 0, left: 0 }}>
-              <img
-                src={Hero1}
-                className='d-block w-100 h-100'
-                alt='Biology Tech 1'
-                style={{ opacity: '0.4', objectFit: 'cover' }}
-              />
-            </div>
-            <div className='carousel-item' style={{ top: 0, left: 0 }}>
-              <img
-                src={Hero2}
-                className='d-block w-100 h-100'
-                alt='Biology Tech 2'
-                style={{ opacity: '0.4', objectFit: 'cover' }}
-              />
-            </div>
-            <div className='carousel-item active' style={{ top: 0, left: 0 }}>
-              <img
-                src={Hero3}
-                className='d-block w-100 h-100'
-                alt='Biology Tech 3'
-                style={{ opacity: '0.4', objectFit: 'cover' }}
-              />
-            </div>
+        <div className='carousel-inner position-relative'>
+          <div className='carousel-item' style={{ top: 0, left: 0 }}>
+            <img
+              src={Hero1}
+              className='d-block w-100 h-100'
+              alt='Biology Tech 1'
+              style={{ opacity: '0.4', objectFit: 'cover' }}
+            />
           </div>
-        </div>
-
-        <div className='position-relative d-flex flex-column justify-content-center align-items-center w-100 h-100 p-3 p-lg-0'>
-          <div className='col-lg-8'>
-            <h1 className='display-4 fw-bold mb-3'>
-              Innovating the Future of Laboratory Science
-            </h1>
-            <p className='lead mb-2 mb-lg-4 fw-semibold'>
-              Explore cutting-edge solutions bridging technology with life
-              sciences, <br />
-              enhancing research precision and efficiency.
-            </p>
-            <div className='position-relative'>
-              <a
-                href='#contact'
-                className='btn btn-contact'
-                style={{
-                  border: '2px solid #1a1a1a',
-                  fontWeight: '600',
-                  borderRadius: '4px',
-                }}
-              >
-                Consult Now
-              </a>
-            </div>
+          <div className='carousel-item' style={{ top: 0, left: 0 }}>
+            <img
+              src={Hero2}
+              className='d-block w-100 h-100'
+              alt='Biology Tech 2'
+              style={{ opacity: '0.4', objectFit: 'cover' }}
+            />
           </div>
-
-          <div className='carousel-indicators position-absolute bottom-0 mb-4 d-none d-md-block'>
-            <button
-              type='button'
-              data-bs-target='#backgroundCarousel'
-              data-bs-slide-to='0'
-              className='active'
-            ></button>
-            <button
-              type='button'
-              data-bs-target='#backgroundCarousel'
-              data-bs-slide-to='1'
-            ></button>
-            <button
-              type='button'
-              data-bs-target='#backgroundCarousel'
-              data-bs-slide-to='2'
-            ></button>
+          <div className='carousel-item active' style={{ top: 0, left: 0 }}>
+            <img
+              src={Hero3}
+              className='d-block w-100 h-100'
+              alt='Biology Tech 3'
+              style={{ opacity: '0.4', objectFit: 'cover' }}
+            />
           </div>
         </div>
       </div>
-    </>
+
+      <div className='position-relative d-flex flex-column justify-content-center align-items-center w-100 h-100 p-3 p-lg-0'>
+        <div className='col-lg-8'>
+          <h1 className='display-4 fw-bold mb-3'>
+            Innovating the Future of Laboratory Science
+          </h1>
+          <p className='lead mb-2 mb-lg-4 fw-semibold'>
+            Explore cutting-edge solutions bridging technology with life
+            sciences, <br />
+            enhancing research precision and efficiency.
+          </p>
+          <div className='position-relative'>
+            <a
+              href='#contact'
+              className='btn btn-contact'
+              style={{
+                border: '2px solid #1a1a1a',
+                fontWeight: '600',
+                borderRadius: '4px',
+              }}
+            >
+              Consult Now
+            </a>
+          </div>
+        </div>
+
+        <div className='carousel-indicators position-absolute bottom-0 mb-4 d-none d-md-block'>
+          <button
+            type='button'
+            data-bs-target='#backgroundCarousel'
+            data-bs-slide-to='0'
+            className='active'
+          ></button>
+          <button
+            type='button'
+            data-bs-target='#backgroundCarousel'
+            data-bs-slide-to='1'
+          ></button>
+          <button
+            type='button'
+            data-bs-target='#backgroundCarousel'
+            data-bs-slide-to='2'
+          ></button>
+        </div>
+      </div>
+    </div>
   );
 };
 
